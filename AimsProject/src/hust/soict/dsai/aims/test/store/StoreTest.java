b15b4cb7b340c248b4b848cf372fca03b3eed50a
thanhleesenpai <thanhle20072004@gmail.com>
2024-11-24 23:02:51 +0700

    bai 8

--- AimsProject/src/hust/soict/dsai/aims/test/store/StoreTest.java
+++ AimsProject/src/hust/soict/dsai/aims/test/store/StoreTest.java
@@ -1,3 +1,7 @@
+package hust.soict.dsai.aims.test.store;
+import hust.soict.dsai.aims.disc.DigitalVideoDisc;
+import hust.soict.dsai.aims.store.Store;
+
 public class StoreTest {
     public static void main(String[] args) {
         DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King", "Animation", "Roger Allers", 87, 19.95f);
